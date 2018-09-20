e6441e45a5242f37ce00b7fae9e2b63bae1d8ff0
MeganKSpence <43014740+MeganKSpence@users.noreply.github.com>
2018-09-20 13:10:04 -0600

    update

---
+++ swapping variebles/index.js
@@ -0,0 +1,18 @@
+console.log('hello world');
+
+// Create 10 variables about yourself
+const firstname = "Megan";
+const lastname = "Spence";
+let age = "14"; //security feature
+const ageAfraid = "18";
+let hair = null; //undefined, null: origionally "none"; return to redish brown
+// debug with typeof
+let female = true;
+
+// Print vairables to the console
+console.log(firstname);
+console.log(lastname);
+console.log(age);
+console.log(ageAfraid);
+console.log(hair);
+console.log("Am I female?" +  true);
