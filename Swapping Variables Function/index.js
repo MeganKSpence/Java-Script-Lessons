4e390d8844be61bc11ef3f46c1f2fe3ef094b2b1
MeganKSpence <43014740+MeganKSpence@users.noreply.github.com>
2018-09-25 13:53:40 -0600

    update

--- Swapping Variables Function/index.js
+++ Swapping Variables Function/index.js
@@ -7,9 +7,15 @@ MeganSpence = ["Megan", "Spence", 14, null, "Reddish Brown", true, false];
 MarkMercer = ["Mark", "Mercer", 13, 29, null, false, true];
 
 // Your code goes here
-Memory= MeganSpence[1]
-MeganSpence[1]= MarkMercer[1]
-MarkMercer[1]= Memory
+function rewritelastnameSpence (lastName1, lastName2) {
+   return MeganSpence[1] = lastname2
+}
+function rewritelastnameMercer (lastname1, lastname2){
+  return MarkMercer[1] = lastname2
+  }
+
+rewritelastnameSpence (MeganSpence[1], MarkMercer[1]);
+rewritelastnameMercer (MarkMercer[1], MeganSpence[1]);
 
 // Print first names only
 
