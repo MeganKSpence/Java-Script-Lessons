a6d97cad1f18b99f7e43334bfabb5e9748a8c07b
Megan Spence <m.spence2@EDU.epsb.ca>
2018-09-26 14:11:18 -0600

    update

--- Operators/index.js
+++ Operators/index.js
@@ -1,16 +1,22 @@
+function simpeOperators (numberA, numberB){
+  let adding = numberA + numberB;
+  let subtract = numberA - numberB
+  let multiply = numberA * numberB
+  let divide = numberA / numberB
+  console.log(adding);
+  console.log(subtract);
+  console.log(multiply);
+  console.log(divide);
+}
+
+let number = [3, 7, 13, 17];
+simpleOperators = (number[0], number[1]);
+simpleOperators = (number[2], number[3]);
 
 
-let number = [4, 7];
-
-let adding = number[0] + number[1];
-let subtract = number[0] - number[1];
-let multiply = number[0] * number [1];
-let divide = number[0] / number[1];
-let skipcount
-
-
-
-console.log(adding);
-console.log(subtract);
-console.log(multiply);
-console.log(divide);
+let skipcount = 15; //skip count by 3's starting at 15
+console.log(skipcount)
+for (let i=1; i<=5; i++){
+  skipcount *= 3;
+  console.log(skipcount);
+}
