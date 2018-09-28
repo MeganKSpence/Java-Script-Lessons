25c0e5bbca27fae78663d0f0db1177b3d590f34d
MeganKSpence <43014740+MeganKSpence@users.noreply.github.com>
2018-09-28 13:36:22 -0600

    update

--- Operators/index.js
+++ Operators/index.js
@@ -1,22 +1,27 @@
 function simpeOperators (numberA, numberB){
   let adding = numberA + numberB;
-  let subtract = numberA - numberB
-  let multiply = numberA * numberB
-  let divide = numberA / numberB
+  let subtract = numberA - numberB;
+  let multiply = numberA * numberB;
+  let divide = numberA / numberB;
   console.log(adding);
   console.log(subtract);
   console.log(multiply);
   console.log(divide);
 }
+//skipcount by ___ for ___ times, starting as ___
+//(skipcount, times, startingnumber) = parameters
+function skipcounting (skipcount, times, startingnumber){
+  let number = startingnumber;
+  for (let i=1; i<=times; i++){
+    //i<=5 = parameter for number of skips
+    number += skipcount; //parameter for skips
+    console.log(number); //parameter for logging rest of the number
+  }
+}
 
 let number = [3, 7, 13, 17];
-simpleOperators = (number[0], number[1]);
-simpleOperators = (number[2], number[3]);
-
-
-let skipcount = 15; //skip count by 3's starting at 15
-console.log(skipcount)
-for (let i=1; i<=5; i++){
-  skipcount *= 3;
-  console.log(skipcount);
-}
+simpleOperators =(number[0], number[1]);
+simpleOperators =(number[2], number[3]);
+skipcounting (717, 313, -900);
+skipcounting (7, 8, 9); //haha 7 eight 9
+skipcounting (8, 8, 0);
