1777fbc4b8b293f8259ad57737aecd70d8447a8f
MeganKSpence <43014740+MeganKSpence@users.noreply.github.com>
2018-10-02 13:15:19 -0600

    update

--- Operators/index.js
+++ Operators/index.js
@@ -19,9 +19,28 @@ function skipcounting (skipcount, times, startingnumber){
   }
 }
 
-let number = [3, 7, 13, 17];
-simpleOperators =(number[0], number[1]);
-simpleOperators =(number[2], number[3]);
-skipcounting (717, 313, -900);
-skipcounting (7, 8, 9); //haha 7 eight 9
-skipcounting (8, 8, 0);
+//simpleComparison
+function howManyTimes (numberStart, numberEnd, skipCount) {
+  while (numberStart < numberEnd) {
+    console.log(numberStart += skipCount); //used to be 2 lines of code ...   console.log(numberStart); numberStart += skipCount;
+    //what is happening with the single line compaired to the double line
+  }
+}
+
+//Truthy vs Falsy example
+console.log(1 == 1); // Returns True
+console.log('1' == 1); // Returns True
+console.log('1' === 1); // Returns False
+
+//let number = [4, 7, 13, 99, 0, 100, 5]
+// while (number < 100) {  //infinite loop ex. 0+5=5, 0+5=5, insted of 0+5=5 5+5=10
+// let number = 0;
+// console.log(number);
+// number += 5;
+// }
+
+//simpleOperators =(number[0], number[1]);
+//simpleOperators =(number[2], number[3]);
+//skipcounting (7, 8, 9); //haha 7 eight 9
+//skipcounting (8, 8, 0);
+//howManyTimes (number[4], number [5], number[6]);
